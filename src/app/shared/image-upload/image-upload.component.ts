050fe1aa4c51a1a8cd53b4bae16ccee2cd1607ef
Marco Karpf <marco.karpf@swisscom.com>
2023-06-24 15:11:39 +0200

    feat (cloudinary) clean image input, make input drag and drop able

--- src/app/shared/image-upload/image-upload.component.ts
+++ src/app/shared/image-upload/image-upload.component.ts
@@ -1,9 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {Observable} from "rxjs";
-import {HttpEventType, HttpResponse} from "@angular/common/http";
-import {CloudinaryService} from "../../cloudinary.service";
-import {CloudinaryImage} from "@cloudinary/url-gen";
-import cloudinary, {Cloudinary} from "cloudinary-core";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-image-upload',
@@ -11,29 +6,52 @@ import cloudinary, {Cloudinary} from "cloudinary-core";
   styleUrls: ['./image-upload.component.scss']
 })
 export class ImageUploadComponent implements OnInit {
-
   currentFile?: File;
   preview = '';
-  imageInfos?: Observable<any>;
+  isDragging = false;
   @Output() fileInputChanged: EventEmitter<File> = new EventEmitter<File>();
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   selectFile(event: any): void {
     if (event.target.files) {
-      this.fileInputChanged.emit(event.target.files[0]);
-      this.currentFile = event.target.files[0];
-      const reader = new FileReader();
-
-      reader.onload = (e: any) => {
-        console.log(e.target.result);
-        this.preview = e.target.result;
-      };
-      reader.readAsDataURL(event.target.files[0]);
+      this.handleFileChange(event.target.files[0]);
     }
+  }
+
+  public handleDrop(event: DragEvent): void {
+    event.preventDefault();
+    this.isDragging = false;
+    if (event.dataTransfer?.files && event.dataTransfer.files.length > 0) {
+      this.handleFileChange(event.dataTransfer.files[0]);
+    }
+  }
+
+  public handleDragOver(event: DragEvent): void {
+    event.preventDefault();
+    this.isDragging = true;
+  }
+
+  public handleDragLeave(event: DragEvent): void {
+    event.preventDefault();
+    this.isDragging = false;
+  }
+
+  private handleFileChange(file: File): void {
+    this.currentFile = file;
+    this.fileInputChanged.emit(file);
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.preview = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  public resetComponent(): void {
+    this.currentFile = undefined;
+    this.preview = '';
+    this.isDragging = false;
   }
 }
